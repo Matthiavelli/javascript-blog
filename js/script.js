57200b995feb59d9d71e65f1c136cc63aa6c0141
Mateusz Durczok <matdur1996@gmail.com>
2022-02-21 16:51:12 +0100

    AddGoodCodeForModule6_4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+{
 'use strict';
 
 const titleClickHandler = function(event){
@@ -37,8 +38,52 @@ const titleClickHandler = function(event){
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+
+  for(let article of articles){
+  
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    console.log('articleId: ', articleId);
+
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    
+    /* get the title from the title element */
+    console.log('articleTitle: ', articleTitle);
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('linkHTML: ' , linkHTML);
+    
+    /* insert link into titleList */
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll('.titles a');
+
+  for(let link of links){
   link.addEventListener('click', titleClickHandler);
+}
+
+}
+
+generateTitleLinks();
+
 }
